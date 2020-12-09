b87490cf763f06d14a0e1166a640e37628e48257
EC2 Default User <ec2-user@ip-172-31-22-97.ec2.internal>
2020-12-09 04:41:04 +0000

    more linting for app

--- app.py
+++ app.py
@@ -22,25 +22,25 @@ from iteration_utilities import unique_everseen
 
 logging.getLogger("werkzeug").setLevel(logging.ERROR)
 
-app = flask.Flask(__name__)
+APP = flask.Flask(__name__)
 
 ##BOILER PLATE CODE TO INITIATE SOCKETS
-socketio = flask_socketio.SocketIO(app)
-socketio.init_app(app, cors_allowed_origins="*")
+SOCKETIO = flask_socketio.SocketIO(APP)
+SOCKETIO.init_app(APP, cors_allowed_origins="*")
 
-dotenv_path = join(dirname(__file__), "sql.env")
-load_dotenv(dotenv_path)
+DOTENV_PATH = join(dirname(__file__), "sql.env")
+load_dotenv(DOTENV_PATH)
 
 # BOILER PLATE CODE TO INSTANTIATE PSQL AND SQLALCHEMY
 
-database_uri = os.environ["DATABASE_URL"]
+DATABASE_URI = os.environ["DATABASE_URL"]
 
-app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
+APP.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 
-db = flask_sqlalchemy.SQLAlchemy(app)
+DB = flask_sqlalchemy.SQLAlchemy(APP)
 
-db.init_app(app)
-db.app = app
+DB.init_app(APP)
+DB.app = APP
 
 
 CALENDER_EVENT_CHANNEL = "calendar_event"
@@ -48,36 +48,36 @@ CALENDER_EVENT_CHANNEL = "calendar_event"
 import models
 
 
-def push_new_user_to_db(ident, name, email):
+def push_new_user_to_database(ident, name, email):
     """
     Pushes new user to database.
     """
-    db.session.add(models.AuthUser(ident, name, email))
-    db.session.commit()
+    DB.session.add(models.AuthUser(ident, name, email))
+    DB.session.commit()
 
 
 def add_event(ccode, title, start, end, desc):
     """
     adds an event, returns id of added event
     """
-    addedEvent = models.Event(ccode, title, start, end, desc)
-    db.session.add(addedEvent)
-    db.session.commit()
-    return addedEvent.id
+    added_event = models.Event(ccode, title, start, end, desc)
+    DB.session.add(added_event)
+    DB.session.commit()
+    return added_event.id
 
 
 def mod_event(ccode, title, start, end, desc, event_id):
     """
     modifies an event, returns id of added event
     """
-    event = db.session.query(models.Event).filter(models.Event.id == event_id).first()
+    event = DB.session.query(models.Event).filter(models.Event.id == event_id).first()
     event.id = event_id
     event.title = title
     event.ccode = ccode
     event.start = start
     event.end = end
     event.desc = desc
-    db.session.commit()
+    DB.session.commit()
     emit_events_to_calender("recieve all events", ccode)
 
 
@@ -85,30 +85,33 @@ def del_event(event_id, ccode):
     """
     Deletes an event and returns the id of the deleted event.
     """
-    db.session.query(models.Event).filter(models.Event.id == event_id).delete()
-    db.session.commit()
+    DB.session.query(models.Event).filter(models.Event.id == event_id).delete()
+    DB.session.commit()
 
 
 def delete_cal(ccode):
+    """
+    Deletes an cal
+    """
     print("Deleting all events!")
     print(":GOT HERE GOT HERE GOT HERE GOT HERE")
     for record in (
-        db.session.query(models.Event).filter(models.Event.ccode.contains(ccode)).all()
+            DB.session.query(models.Event).filter(models.Event.ccode.contains(ccode)).all()
     ):
         print(record)
-    db.session.query(models.Calendars).filter(models.Calendars.ccode == ccode).delete()
-    db.session.commit()
+    DB.session.query(models.Calendars).filter(models.Calendars.ccode == ccode).delete()
+    DB.session.commit()
 
 
-def add_calendar_for_user(userid, privFlag):
+def add_calendar_for_user(userid, priv_flag):
 
     """
     adds an event, returns the ccode of the new calendar
     """
-    addedCalendar = models.Calendars(userid, privFlag)
-    db.session.add(addedCalendar)
-    db.session.commit()
-    return addedCalendar.ccode
+    added_calendar = models.Calendars(userid, priv_flag)
+    DB.session.add(added_calendar)
+    DB.session.commit()
+    return added_calendar.ccode
 
 
 def get_sid():
@@ -133,7 +136,7 @@ def emit_events_to_calender(channel, cal_code):
                 "ccode": record.ccode,
                 "eventid": record.id,
             }
-            for record in db.session.query(models.Event)
+            for record in DB.session.query(models.Event)
             .filter(models.Event.ccode.contains([ccode]))
             .all()
         ]
@@ -142,7 +145,7 @@ def emit_events_to_calender(channel, cal_code):
     all_events = list(unique_everseen(all_events))
     # for event in all_events:
     #     print(event)
-    socketio.emit(channel, all_events, room=sid)
+    SOCKETIO.emit(channel, all_events, room=sid)
 
 
 def rfc3339_to_unix(timestamp):
@@ -162,7 +165,7 @@ def emit_ccode_to_calender(channel, ccodes):
     ccode_details = {}
     for ccode in ccodes:
         record = (
-            db.session.query(models.Calendars)
+            DB.session.query(models.Calendars)
             .filter(models.Calendars.ccode == ccode)
             .first()
         )
@@ -173,7 +176,7 @@ def emit_ccode_to_calender(channel, ccodes):
         ccode_details[record.ccode] = details_for_ccode
 
     if len(ccode_details) > 0:
-        socketio.emit(channel, ccode_details, room=sid)
+        SOCKETIO.emit(channel, ccode_details, room=sid)
 
 
 def exists_in_auth_user(check_id):
@@ -181,7 +184,7 @@ def exists_in_auth_user(check_id):
     Check to see if the auth user is there
     """
     return (
-        db.session.query(models.AuthUser.userid).filter_by(userid=check_id).scalar()
+        DB.session.query(models.AuthUser.userid).filter_by(userid=check_id).scalar()
         is not None
     )
 
@@ -191,13 +194,13 @@ def exists_in_calender(merge_code):
     Check to see if merge calendar exists
     """
     return (
-        db.session.query(models.Calendars.ccode).filter_by(ccode=merge_code).scalar()
+        DB.session.query(models.Calendars.ccode).filter_by(ccode=merge_code).scalar()
         is not None
     )
 
 
 ##SOCKET EVENTS
-@socketio.on("connect")
+@SOCKETIO.on("connect")
 def on_connect():
     """
     Runs on connect.
@@ -205,7 +208,7 @@ def on_connect():
     # print("Someone connected!")
 
 
-@socketio.on("disconnect")
+@SOCKETIO.on("disconnect")
 def on_disconnect():
     """
     Runs on disconnect.
@@ -213,7 +216,7 @@ def on_disconnect():
     # print("Someone disconnected!")
 
 
-@socketio.on("new google user")
+@SOCKETIO.on("new google user")
 def on_new_google_user(data):
     """
     Runs verification on google token.
@@ -232,15 +235,15 @@ def on_new_google_user(data):
         name = credentials.id_token["name"]
         exists = exists_in_auth_user(userid)
         if not exists:
-            push_new_user_to_db(userid, name, email)
+            push_new_user_to_database(userid, name, email)
             add_calendar_for_user(userid, False)
         all_ccodes = [
             record.ccode
-            for record in db.session.query(models.Calendars)
+            for record in DB.session.query(models.Calendars)
             .filter_by(userid=userid)
             .all()
         ]
-        socketio.emit(
+        SOCKETIO.emit(
             "Verified",
             {
                 "name": name,
@@ -264,7 +267,7 @@ def on_new_google_user(data):
         return "Unverified."
 
 
-@socketio.on("delete calendar")
+@SOCKETIO.on("delete calendar")
 def on_delete_cal(data):
     """
     add a new event for to calendar
@@ -273,7 +276,7 @@ def on_delete_cal(data):
     delete_cal(data["ccode"])
 
 
-@socketio.on("add calendar")
+@SOCKETIO.on("add calendar")
 def on_add_calendar(data):
     """
     add a new calendar for user
@@ -300,7 +303,7 @@ def on_add_calendar(data):
     ccode_list.append(ccode)
     emit_events_to_calender("recieve all events", ccode_list)
 
-    socketio.emit(
+    SOCKETIO.emit(
         "update dropdown",
         {
             "ccode": ccode,
@@ -308,7 +311,7 @@ def on_add_calendar(data):
     )
 
 
-@socketio.on("get events")
+@SOCKETIO.on("get events")
 def send_events_to_calendar(data):
     """
     send_events_to_calendar.
@@ -319,7 +322,7 @@ def send_events_to_calendar(data):
     print("SENT EVENTS!")
 
 
-@socketio.on("get ccode details")
+@SOCKETIO.on("get ccode details")
 def send_ccode_to_calendar(data):
     """
     send_ccode_to_calendar.
@@ -329,7 +332,7 @@ def send_ccode_to_calendar(data):
     print("SENT ccode!")
 
 
-@socketio.on("new event")
+@SOCKETIO.on("new event")
 def on_new_event(data):
     """
     add a new event for to calendar
@@ -343,7 +346,7 @@ def on_new_event(data):
     print(end)
     added_event_id = add_event([ccode], title, start, end, "some words")
     print("SENDING INDIVIDUAL EVENT")
-    socketio.emit(
+    SOCKETIO.emit(
         "calender_event",
         {
             "title": title,
@@ -357,7 +360,7 @@ def on_new_event(data):
     return added_event_id
 
 
-@socketio.on("modify event")
+@SOCKETIO.on("modify event")
 def on_modify_event(data):
     """
     add a new event for to calendar
@@ -378,7 +381,7 @@ def on_modify_event(data):
     emit_events_to_calender("recieve all events", ccode_list)
 
 
-@socketio.on("delete event")
+@SOCKETIO.on("delete event")
 def on_delete_event(data):
     """
     add a new event for to calendar
@@ -394,7 +397,7 @@ def on_delete_event(data):
     emit_events_to_calender("recieve all events", ccode_list)
 
 
-@socketio.on("cCodeToMerge")
+@SOCKETIO.on("cCodeToMerge")
 def on_merge_calendar(data):
     """
     merge calendar
@@ -408,13 +411,13 @@ def on_merge_calendar(data):
         if not exists:
             raise ValueError
         for record in (
-            db.session.query(models.Event)
-            .filter(models.Event.ccode.contains([merge_code]))
-            .all()
+                DB.session.query(models.Event)
+                .filter(models.Event.ccode.contains([merge_code]))
+                .all()
         ):
             if cal_code not in record.ccode:
                 record.ccode.append(cal_code)
-                db.session.commit()
+                DB.session.commit()
 
         print("cal_code appended")
         ccode_list.append(merge_code)
@@ -427,7 +430,7 @@ def on_merge_calendar(data):
         )
 
 
-@socketio.on("Import Calendar")
+@SOCKETIO.on("Import Calendar")
 def on_import_calendar(data):
     """
     import primary google calendar for user
@@ -451,8 +454,8 @@ def on_import_calendar(data):
     ccode_list.append(ccode)
     for event in events:
         if (
-            event["start"].get("dateTime") is None
-            or event["end"].get("dateTime") is None
+                event["start"].get("dateTime") is None
+                or event["end"].get("dateTime") is None
         ):
             continue
         start = int(rfc3339_to_unix(str(event["start"].get("dateTime"))))
@@ -473,10 +476,10 @@ def on_import_calendar(data):
             )
         except KeyError:
             desc = "some desc"
-        addedEventId = add_event([ccode], title, start, end, desc)
-        print(addedEventId)
+        added_event_id = add_event([ccode], title, start, end, desc)
+        print(added_event_id)
     emit_events_to_calender("recieve all events", ccode_list)
-    socketio.emit(
+    SOCKETIO.emit(
         "update dropdown",
         {
             "ccode": ccode,
@@ -484,58 +487,58 @@ def on_import_calendar(data):
     )
 
 
-@socketio.on("modify calendar")
+@SOCKETIO.on("modify calendar")
 def on_modify_calendar(data):
     """
-    modify calendars
+    modify calendar
     """
     print(data)
     ccode = data["ccode"]
     private = data["privateCal"]
-    delete_cal = data["deleteCal"]
+    del_cal = data["deleteCal"]
     all_ccodes = data["allCcodes"]
 
     print(ccode)
     calendar = (
-        db.session.query(models.Calendars)
+        DB.session.query(models.Calendars)
         .filter(models.Calendars.ccode == ccode)
         .first()
     )
     if calendar:
-        if delete_cal:
+        if del_cal:
             for record in (
-                db.session.query(models.Event)
-                .filter(models.Event.ccode.contains([ccode]))
-                .all()
+                    DB.session.query(models.Event)
+                    .filter(models.Event.ccode.contains([ccode]))
+                    .all()
             ):
                 if record.ccode[0] == ccode:
                     del_event(record.id, ccode)
-            db.session.query(models.Calendars).filter(
+            DB.session.query(models.Calendars).filter(
                 models.Calendars.ccode == ccode
             ).delete()
         elif private:
             calendar.private = True
         elif private:
             calendar.private = False
-        db.session.commit()
+        DB.session.commit()
         emit_events_to_calender("recieve all events", all_ccodes)
 
 
-@app.route("/")
+@APP.route("/")
 def hello():
     """
     Runs at page-load.
 
     """
-    models.db.create_all()
-    db.session.commit()
+    models.DB.create_all()
+    DB.session.commit()
     print("User has joined.")
     return flask.render_template("index.html")
 
 
 if __name__ == "__main__":
-    socketio.run(
-        app,
+    SOCKETIO.run(
+        APP,
         host=os.getenv("IP", "0.0.0.0"),
         port=int(os.getenv("PORT", "8080")),
         debug=True,
